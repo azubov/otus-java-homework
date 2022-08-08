33fcbf4d8e5cf29483bbce81aa1029b52e3f1130
Зубов Артем <zubov@lanit.ru>
2022-08-08 23:41:38 +0300

    hw05-AOP annotatedMethods added

--- hw05-AOP/src/main/java/ru/otus/proxy/Ioc.java
+++ hw05-AOP/src/main/java/ru/otus/proxy/Ioc.java
@@ -7,6 +7,8 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class Ioc {
 
@@ -21,26 +23,31 @@ public class Ioc {
 
         private static final String LOG_MSG = "executed method: %s, param: %s";
         private final T myClass;
-        private final Class<? extends Annotation> annotation;
+        private final List<Method> annotatedMethods;
 
         LogInvocationHandler(final T myClass, final Class<? extends Annotation> annotation) {
             this.myClass = myClass;
-            this.annotation = annotation;
+            this.annotatedMethods = getAnnotatedMethods(myClass, annotation);
+        }
+
+        private List<Method> getAnnotatedMethods(final T myClass, final Class<? extends Annotation> annotation) {
+            return Arrays.stream(myClass.getClass().getMethods()).filter(m -> m.isAnnotationPresent(annotation)).toList();
         }
 
         @Override
         public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-            if (hasAnnotation(method)) return logging(myClass, method, args);
+            if (isAnnotated(method)) {
+                return logging(myClass, method, args);
+            }
             return method.invoke(myClass, args);
         }
 
-        private boolean hasAnnotation(final Method method) throws NoSuchMethodException {
-            var methodImpl = getImpl(method);
-            return methodImpl.isAnnotationPresent(annotation);
-        }
-
-        private Method getImpl(final Method method) throws NoSuchMethodException {
-            return myClass.getClass().getMethod(method.getName(), method.getParameterTypes());
+        private boolean isAnnotated(final Method method) {
+            Predicate<Method> signature = m ->
+                    m.getName().equals(method.getName())
+                    && Arrays.toString(m.getParameterTypes()).equals(Arrays.toString(method.getParameterTypes()))
+                    && m.getReturnType().equals(method.getReturnType());
+            return annotatedMethods.stream().anyMatch(signature);
         }
 
         private Object logging(final T myClass, final Method method, final Object[] args) throws Throwable {
