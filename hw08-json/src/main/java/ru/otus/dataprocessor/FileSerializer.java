559c0da3c722ae3b68943c484be7450f6a49713a
Зубов Артем <zubov@lanit.ru>
2022-09-10 21:55:19 +0300

    hw08 solution

--- hw08-json/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,25 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    private final String path;
+    private final Gson mapper = new Gson();
+
     public FileSerializer(String fileName) {
+        path = fileName;
     }
 
     @Override
-    public void serialize(Map<String, Double> data) {
+    public void serialize(Map<String, Double> data) throws IOException {
         //формирует результирующий json и сохраняет его в файл
+        try (var writer = new FileWriter(path)) {
+            mapper.toJson(data, writer);
+        }
     }
 }
--- hw08-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,26 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String path;
+    private final Gson mapper = new Gson();
+
     public ResourcesFileLoader(String fileName) {
+        path = ClassLoader.getSystemResource(fileName).getPath();
     }
 
     @Override
-    public List<Measurement> load() {
+    public List<Measurement> load() throws IOException {
         //читает файл, парсит и возвращает результат
-        return null;
+        var toType = TypeToken.getParameterized(List.class, Measurement.class).getType();
+        return mapper.fromJson(new FileReader(path), toType);
     }
 }
