17da41df8468f78ba18122f52fc2b5c772023c08
Зубов Артем <zubov@lanit.ru>
2023-04-09 14:14:41 +0400

    hw15 init

---
+++ hw15-executors/src/main/java/ru/otus/ExecutorsMain.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExecutorsMain {
+
+    private static final Logger log = LoggerFactory.getLogger(ExecutorsMain.class);
+
+    public static void main(String... args) {
+
+    }
+}
